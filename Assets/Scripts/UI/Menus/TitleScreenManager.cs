3ececf813ecb66bfe8f4d554ec4d8dff75728486
Carlos Marques <catralitos11@gmail.com>
2023-07-08 16:56:52 +0100

    Added score HUD

--- Assets/Scripts/UI/Menus/TitleScreenManager.cs
+++ Assets/Scripts/UI/Menus/TitleScreenManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Menus
+namespace UI.Menus
 {
     /// <summary>
     /// This class manages the title screen.
