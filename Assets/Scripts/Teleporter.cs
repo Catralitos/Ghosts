055681d7f91de3896e67fff24b72e1941eb71538
Carlos Marques <catralitos11@gmail.com>
2023-07-08 17:33:58 +0100

    Added teleporter prefab

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,20 @@
+using Extensions;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public LayerMask teleportables;
+
+    public int teleporterDistance;
+    public bool rightTeleporter;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (teleportables.HasLayer(other.gameObject.layer))
+        {
+            int multiplier = rightTeleporter ? -1 : 1;
+            GameObject o = other.gameObject;
+            o.transform.position += new Vector3(multiplier * teleporterDistance, 0, 0);
+        }
+    }
+}
