b485ec7054098cb99230cbd7c86af406ac4f6311
Carlos Marques <catralitos11@gmail.com>
2023-07-09 01:11:56 +0100

    Removed unused code and adjusted folders/namespaces

--- Assets/Scripts/Ghosts/GhostScatter.cs
+++ Assets/Scripts/Ghosts/GhostScatter.cs
@@ -1,15 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using Maze_Elements;
 using UnityEngine;
 
-public class GhostScatter : MonoBehaviour
+namespace Ghosts
 {
-    private void OnTriggerEnter2D(Collider2D other) {
-        Node node = other.GetComponent<Node>();
+    public class GhostScatter : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other) {
+            Node node = other.GetComponent<Node>();
         
-        if (node != null && this.enabled)
-        {
-            int index = Random.Range(0, node.availableDirections.Count);
+            if (node != null && this.enabled)
+            {
+                int index = Random.Range(0, node.availableDirections.Count);
+            }
         }
     }
 }
